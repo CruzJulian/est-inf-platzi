2c67c5d2049a7db1e35bb365c7d4d2fa205a76e6
Julián Cruz <cruz.julian.mauricio@gmail.com>
2020-08-01 19:03:36 -0500

    correcciones

--- sim_bootstrapping.r
+++ sim_bootstrapping.r
@@ -1,14 +1,11 @@
 
-# Hacer bootstrapping sobre nuestra regresión lineal. ---------------------
+
+# Hacemos un bootstrapping para nuestra regresión lineal. -----------------
+
 
 
 # Paquetes ----------------------------------------------------------------
 
-
-library("dplyr")
-library("purrr")
-library("tidyr")
-library("ggplot2")
 
 # Bootstrapping -----------------------------------------------------------
 
@@ -17,81 +14,150 @@ tamano_muestral <- 23
 iteraciones <- 1000
 beta_0 <- 1
 beta_1 <- -0.3
-
+desv_est_error <- 0.5
 
 genera_x <- function(n) seq(-3, 3, length.out = n)
 
 genera_y <- function(x, beta_0, beta_1){
-  beta_1*x + beta_0 + rnorm(length(x), 0, 0.5)
+  beta_1*x + beta_0 + rnorm(length(x), 0, desv_est_error)
 }
 
-tibble(
-  datos_x = genera_x(tamano_muestral),
-  datos_y = genera_y(datos_x, beta_0, beta_1)
-) -> datos_simulados
+datos_x <- genera_x(tamano_muestral)
+datos_y <- genera_y(datos_x, beta_0, beta_1)
 
 
-lm(datos_y ~ datos_x, data = datos_simulados) -> modelo
+lm(datos_y ~ datos_x) -> modelo
 coefficients(modelo) -> coeficientes_muestrales
 summary(modelo)
 confint(modelo)
 
+beta_0_estimado <- beta_1_estimado <- vector()
 
-coeficientes <- list() #vamos a guardar aquí los coeficientes de una regresión
-
-for(i in 1:iteraciones){
-  muestra <- sample_n(datos_simulados, nrow(datos_simulados), replace = TRUE)
-  lm(datos_y ~ datos_x, data = muestra) -> modelo
-  coeficientes[[i]] <- coefficients(modelo)
+for(i in seq_len(iteraciones)){
+  muestra <- sample(seq_along(datos_x), length(datos_x), replace = TRUE)
+  muestra_x <- datos_x[muestra]
+  muestra_y <- datos_y[muestra]
+  lm(muestra_y ~ muestra_x) -> modelo
+  coeficientes <- coefficients(modelo)
+  beta_0_estimado[i] <- coeficientes[1]
+  beta_1_estimado[i] <- coeficientes[2]
 }
 
 
-coeficientes %>% 
-  transpose %>% 
-  lapply(unlist) %>% 
-  as_tibble() %>% 
-  gather(key = coeficiente, value = valor) %>% 
-  group_by(coeficiente) %>% 
-  summarise(
-    LI = quantile(valor, 0.025),
-    LS = quantile(valor, 0.975)
-  )
+data.frame(
+  limite = c("LI", "LS"),
+  beta_0 = quantile(beta_0_estimado, c(0.025, 0.975)),
+  beta_1 = quantile(beta_1_estimado, c(0.025, 0.975))
+) -> intervalo_bootstrapping
 
+intervalo_bootstrapping
 
+# data.frame(
+#   beta_0_estimado,
+#   beta_1_estimado
+# ) %>% 
+#   gather(key = coeficiente, value = valor) %>% 
+#   group_by(coeficiente) %>% 
+#   summarise(
+#     LI = quantile(valor, 0.025),
+#     LS = quantile(valor, 0.975)
+#   )
+# 
 
-plot(beta_0, beta_1, ylim = c(-0.5, 0), xlim = c(0.7, 1.6))
+xmin_gr <- beta_0 - 10*desv_est_error/sqrt(tamano_muestral)
+xmax_gr <- beta_0 + 10*desv_est_error/sqrt(tamano_muestral)
+ymin_gr <- beta_1 - 10*desv_est_error/sqrt(tamano_muestral)
+ymax_gr <- beta_1 + 10*desv_est_error/sqrt(tamano_muestral)
+
+plot(beta_0, beta_1, xlim = c(xmin_gr, xmax_gr), ylim = c(ymin_gr, ymax_gr))
 
 for(i in 1:iteraciones){
-points(coeficientes[[i]][1], coeficientes[[i]][2])
+points(beta_0_estimado[i], beta_1_estimado[i])
 }
 points(coeficientes_muestrales[1], coeficientes_muestrales[2], pch = 20, col = 4, cex = 3)
 points(beta_0, beta_1, pch = 20, col = 2, cex = 3)
+rect(
+  intervalo_bootstrapping$beta_0[1], 
+  intervalo_bootstrapping$beta_1[1],
+  intervalo_bootstrapping$beta_0[2],
+  intervalo_bootstrapping$beta_1[2],
+  border = 4, lwd = 2)
 
 
-# Gráfico bonito ----------------------------------------------------------
 
+# Tidy approach -----------------------------------------------------------
+
+
+library("dplyr")
+library("purrr")
+library("tidyr", exclude = c("extract"))
+library("ggplot2")
+library("magrittr")
+
+colores_platzi <- c("#78D92A", "#002E4E", "#058ECD", "#ED2B05", "#F4F7F4")
+
+tamano_muestral <- 23
+iteraciones <- 1000
+beta_0 <- 1
+beta_1 <- -0.3
+desv_est_error <- 0.5
+
+genera_x <- function(n) seq(-3, 3, length.out = n)
+
+genera_y <- function(x, beta_0, beta_1){
+  beta_1*x + beta_0 + rnorm(length(x), 0, desv_est_error)
+}
+
+datos_simulados <- tibble(
+  x = genera_x(tamano_muestral),
+  y = genera_y(datos_x, beta_0, beta_1)
+)
+
+
+lm(y ~ x, data = datos_simulados) -> modelo
+coefficients(modelo) -> coeficientes_muestrales
+summary(modelo)
+confint(modelo)
+
+tibble(
+  muestras = replicate(iteraciones, sample_n(datos_simulados, nrow(datos_simulados), replace = TRUE), simplify = FALSE),
+  modelos = map(muestras, function(muestra){lm(y~x, data = muestra)}),
+  beta_0_estimado = map(modelos, coef) %>% map_dbl(extract, 1),
+  beta_1_estimado = map(modelos, coef) %>% map_dbl(extract, 2)
+) -> coeficientes
 
 coeficientes %>% 
-  transpose %>% 
-  lapply(unlist) %>% 
-  as_tibble() %>% 
+  select(beta_0_estimado, beta_1_estimado) %>% 
+    gather(key = coeficiente, value = valor) %>%
+    group_by(coeficiente) %>%
+    summarise(
+      LI = quantile(valor, 0.025),
+      LS = quantile(valor, 0.975)
+    ) -> intervalo_bootstrap
+
+coeficientes %>% 
+  select(beta_0_estimado, beta_1_estimado) %>% 
   gather(key = coeficiente, value = valor) %>% 
   ggplot +
   aes(x = valor) + 
   geom_density(fill = "#78D92A", alpha = 0.5, colour = "#78D92A") +
-  facet_wrap(~coeficiente, nrow = 4,  scales = "free") + theme_light()
+  facet_wrap(~coeficiente, nrow = 4,  scales = "free") + 
+  theme_minimal()
 
-coeficientes %>% 
-  transpose %>% 
-  lapply(unlist) %>% 
-  as_tibble(.name_repair = "universal") %>% 
-  # gather(key = coeficiente, value = valor) %>% 
-  ggplot +
-  aes(x = .Intercept., y = datos_x) + 
-  geom_point() +
-  annotate("point",x = beta_0, y = beta_1, colour = "#ED2B05", size = 5) +
-  annotate("point",x = coeficientes_muestrales[1], y = coeficientes_muestrales[2], colour = "#058ECD", size = 5) +
-  theme_light()
+ggplot() +
+  geom_point(
+    data = coeficientes, 
+    mapping = aes(x = beta_0_estimado, y = beta_1_estimado)
+    ) +
+  geom_rect(
+    data = intervalo_bootstrap, 
+    mapping = aes(xmin = LI[[1]], ymin = LI[[2]], xmax = LS[[1]], ymax = LS[[2]]),
+    fill = colores_platzi[1],
+    alpha = 0.2
+    ) +
+  annotate("point",x = beta_0, y = beta_1, colour = colores_platzi[4], size = 5) +
+  annotate("point",x = coeficientes_muestrales[1], y = coeficientes_muestrales[2], colour = colores_platzi[3], size = 5) +
+  theme_minimal()
 
 
 
