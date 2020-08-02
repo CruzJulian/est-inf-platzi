7ab2411732472c36aee30b6fa0e1750ee3d7b071
Julián Cruz <cruz.julian.mauricio@gmail.com>
2020-08-01 19:54:57 -0500

    Correcciones

--- sim_cross_val.r
+++ sim_cross_val.r
@@ -6,14 +6,12 @@
 # Paquetes ----------------------------------------------------------------
 
 library("caret")
-library("dplyr")
 library("nnet")
-library("purrr")
-
+library("parallel")
 
 # función de pliegue ------------------------------------------------------
 
-rmse_fold <- function(pliegue, X, Y){
+rmse_por_pliegue <- function(pliegue, X, Y){
   entrena_X <- X[-pliegue]
   entrena_Y <- Y[-pliegue]
   modelo <- nnet(X, Y, size = 8, linout = TRUE, trace = FALSE)
@@ -25,27 +23,59 @@ rmse_fold <- function(pliegue, X, Y){
 # Red neuronal ------------------------------------------------------------
 
 
-n_pliegues <- 10
-tamano_muestral <- 23
+n_pliegues <- 5
+tamano_muestral <- 3000
 
 genera_y <- function(x){
   cos(x) + rnorm(length(x), 0, 0.5)
-  # beta_1*x + beta_0 + rnorm(length(x), 0, 0.5)
 }
-
-
-rmse_modelo <- vector()
 
 
 X <- seq(0, 3*pi, length.out = tamano_muestral)
 Y <- genera_y(X)
 
 
-
 createFolds(Y, k = n_pliegues) -> pliegues
 
+mclapply(
+  pliegues,
+  rmse_por_pliegue,
+  X,
+  Y,
+  mc.cores = floor(detectCores()*0.8)
+  ) -> rmse_pliegues 
 
-pliegues %>% 
-  map_dbl(rmse_fold, X, Y) %>% 
-  mean 
+rmse_pliegues <- unlist(rmse_pliegues)
 
+plot(rmse_pliegues, ylim = c(0, 1))
+
+mean(rmse_pliegues)
+
+
+# Tidy approach -----------------------------------------------------------
+
+library("dplyr")
+library("magrittr")
+
+n_pliegues <- 5
+tamano_muestral <- 3000
+
+tibble(
+  pliegues = createFolds(Y, k = n_pliegues),
+  rmse_pliegues = mclapply(
+    pliegues,
+    rmse_por_pliegue,
+    X,
+    Y,
+    mc.cores = floor(detectCores()*0.8)
+  ) %>% unlist,
+  nombres = names(pliegues)
+) -> validacion
+
+validacion %$% mean(rmse_pliegues)
+
+ggplot(validacion) +
+  geom_vline(aes(xintercept = 0), size = 1.5) +
+  geom_segment(aes(x = 0, y = nombres, xend = rmse_pliegues, yend = nombres), colour = "grey75") +
+  geom_point(aes(x = rmse_pliegues, y = nombres), size = 4) +
+  theme_minimal()
