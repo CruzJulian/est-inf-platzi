4d862385c21035a8dadea36f1f0d1843d6a30802
Julián Cruz <cruz.julian.mauricio@gmail.com>
2020-08-01 21:16:15 -0500

    Correcciones

--- proy_tam_muestral.r
+++ proy_tam_muestral.r
@@ -8,6 +8,7 @@ library("dplyr")
 library("saber")
 library("nnet")
 library("caret")
+library("parallel")
 library("purrr")
 library("magrittr")
 
@@ -25,14 +26,15 @@ rmse_fold <- function(pliegue, X, Y, nn_size){
   rmse <- sqrt(mean((unlist(subset(Y, pliegue_logic)) - Y_pronosticado)^2))
   rmse
 }
-# pliegues[[1]] -> pliegue
-# nn_size <- 10
 
 # Red neuronal ------------------------------------------------------------
 
 
 tamano_muestral <- 2000
 n_pliegues <- 10
+tamanos_muestrales <- seq(2000, 10000, by = 200)
+n_pliegues <- 20
+neuronas <- 4
 
 c(
   "ECON_SN_TELEFONIA", 
@@ -54,67 +56,29 @@ Y <- muestra[nombre_Y]
 
 Y_v <- unlist(Y)
 
-neuronas <- 4
-
-# red_neuronal <- nnet(X, Y, size = neuronas, linout = TRUE, trace = FALSE)
-
-# plot(Y_v ~ predict(red_neuronal))
-# lines(1:100, col = 2)
-
+neuronas <- 8
 
 createFolds(Y_v, k = n_pliegues) -> pliegues
 
-
-pliegues %>% 
-  map_dbl(rmse_fold, X, Y, nn_size = neuronas) %>% 
-  mean
-
-
-tamanos_muestrales <- seq(50, 10000, by = 50)
-
-
-n_pliegues <- 20
-neuronas <- 4
-
 calcula_rmse_tam <- function(tamano_muestral){
   
-    
   SB11_20112 %>% 
     sample_n(tamano_muestral) %>% 
     extract(c(nombres_X, nombre_Y)) %>% 
     na.omit() -> muestra
   
   X <- muestra[nombres_X]
-  
   Y <- muestra[nombre_Y]
-  
   Y_v <- unlist(Y)
   
-  
   createFolds(Y_v, k = n_pliegues) -> pliegues
-  
   
   pliegues %>% 
     map_dbl(rmse_fold, X, Y, nn_size = neuronas) %>% 
     mean 
-
   
 }
 
-
-system.time(
-  lapply(tamanos_muestrales, calcula_rmse_tam) -> rmse_por_tam
-)
-
-
-plot(tamanos_muestrales, rmse_por_tam)
-
-library("parallel")
-numCores <- detectCores()
-numCores
-
-system.time(
-  mclapply(tamanos_muestrales, calcula_rmse_tam, mc.cores = 6) -> rmse_por_tam
-)
+mclapply(tamanos_muestrales, calcula_rmse_tam, mc.cores = floor(detectCores()*0.8)) -> rmse_por_tam
 
 plot(tamanos_muestrales, rmse_por_tam)
