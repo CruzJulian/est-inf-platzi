7713739868068be2608a3ee7654dc443ac918397
Julián Cruz <cruz.julian.mauricio@gmail.com>
2020-08-05 20:46:07 -0500

    Perfeccionamiento
    Últimos detalles

--- proy_estima_inter.r
+++ proy_estima_inter.r
@@ -19,48 +19,16 @@ table(SB11_20112$ECON_SN_INTERNET)
 
 
 tamano_muestral <- 30
-
-muestra <- sample(seq_len(nrow(SB11_20112)), tamano_muestral)
-
-pobla_A <- SB11_20112$FISICA_PUNT[SB11_20112$ECON_SN_INTERNET == 0]
-cuales_A <- seq_len(nrow(SB11_20112)) %in% muestra & SB11_20112$ECON_SN_INTERNET == 0
-muestra_A <- SB11_20112$FISICA_PUNT[cuales_A]
-
-pobla_B <- SB11_20112$FISICA_PUNT[SB11_20112$ECON_SN_INTERNET == 1]
-cuales_B <- seq_len(nrow(SB11_20112)) %in% muestra & SB11_20112$ECON_SN_INTERNET == 1
-muestra_B <- SB11_20112$FISICA_PUNT[cuales_B]
-
-
-media_pob_A <- mean(pobla_A, na.rm = TRUE)
-media_muestra_A <- mean(muestra_A, na.rm = TRUE)
-min_A <- media_muestra_A - 10*sd(muestra_A, na.rm = TRUE)/sqrt(tamano_muestral)
-max_A <- media_muestra_A + 10*sd(muestra_A, na.rm = TRUE)/sqrt(tamano_muestral)
-t_test_A <- t.test(muestra_A)
-intervalo_A <- t_test_A$conf.int
-LI_A <- min(intervalo_A)
-LS_A <- max(intervalo_A)
-
-media_pob_B <- mean(pobla_B, na.rm = TRUE)
-media_muestra_B <- mean(muestra_B, na.rm = TRUE)
-min_B <- media_muestra_B - 10*sd(muestra_B, na.rm = TRUE)/sqrt(tamano_muestral)
-max_B <- media_muestra_B + 10*sd(muestra_B, na.rm = TRUE)/sqrt(tamano_muestral)
-t_test_B <- t.test(muestra_B, na.rm = TRUE)
-intervalo_B <- t_test_B$conf.int
-LI_B <- min(intervalo_B)
-LS_B <- max(intervalo_B)
-
-plot(media_pob_A, media_pob_B, xlim = c(min_A, max_A), ylim = c(min_B, max_B), col = 4, pch = 20)
-points(media_muestra_A, media_muestra_B, col = 2, pch = 20)
-abline(1,1)
-rect(LI_A, LI_B, LS_A, LS_B)
-
-
-
-
 iteraciones <- 100
 
-  plot(media_pob_A, media_pob_B, xlim = c(min_A, max_A), ylim = c(min_B, max_B), col = 4, pch = 20)
-  abline(1,1)
+
+pobla_A <- SB11_20112$FISICA_PUNT[SB11_20112$ECON_SN_INTERNET == 0]
+pobla_B <- SB11_20112$FISICA_PUNT[SB11_20112$ECON_SN_INTERNET == 1]
+media_pob_A <- mean(pobla_A, na.rm = TRUE)
+media_pob_B <- mean(pobla_B, na.rm = TRUE)
+
+plot(media_pob_A, media_pob_B, col = 4, pch = 20)
+abline(0,1)
 
 for(i in seq_len(iteraciones)){
   
@@ -71,8 +39,6 @@ for(i in seq_len(iteraciones)){
   
   cuales_B <- seq_len(nrow(SB11_20112)) %in% muestra & SB11_20112$ECON_SN_INTERNET == 1
   muestra_B <- SB11_20112$FISICA_PUNT[cuales_B]
-  
-  
   
   media_muestra_A <- mean(muestra_A, na.rm = TRUE)
   t_test_A <- t.test(muestra_A)
@@ -86,13 +52,13 @@ for(i in seq_len(iteraciones)){
   LI_B <- min(intervalo_B)
   LS_B <- max(intervalo_B)
   
-  points(media_muestra_A, media_muestra_B, col = 2, pch = 20)
+  # points(media_muestra_A, media_muestra_B, col = 2, pch = 20)
   rect(LI_A, LI_B, LS_A, LS_B)
   
 }
 
 
-points(media_pob_A, media_pob_B, col = 4, pch = 20, cex = 3)
+points(media_pob_A, media_pob_B, col = 4, pch = 20, cex = 2)
   
 
 
@@ -130,22 +96,22 @@ tibble(
 ) -> analisis
 
 media_pob_A <- mean(pobla_A, na.rm = TRUE)
-min_A <- media_pob_A - 15*sd(pobla_A, na.rm = TRUE)/sqrt(tamano_muestral)
-max_A <- media_muestra_A + 15*sd(pobla_A, na.rm = TRUE)/sqrt(tamano_muestral)
+# min_A <- media_pob_A - 15*sd(pobla_A, na.rm = TRUE)/sqrt(tamano_muestral)
+# max_A <- media_muestra_A + 15*sd(pobla_A, na.rm = TRUE)/sqrt(tamano_muestral)
 
 media_pob_B <- mean(pobla_B, na.rm = TRUE)
-min_B <- media_pob_B - 15*sd(pobla_B, na.rm = TRUE)/sqrt(tamano_muestral)
-max_B <- media_pob_B + 15*sd(pobla_B, na.rm = TRUE)/sqrt(tamano_muestral)
+# min_B <- media_pob_B - 15*sd(pobla_B, na.rm = TRUE)/sqrt(tamano_muestral)
+# max_B <- media_pob_B + 15*sd(pobla_B, na.rm = TRUE)/sqrt(tamano_muestral)
 
 
 analisis %>% 
   ggplot +
   geom_rect(aes(xmin = LI_A, xmax = LS_A, ymin = LI_B, ymax = LS_B), alpha = 0.2, fill = colores[6]) +
-  geom_point(aes(x = media_muestra_A, y = media_muestra_B), colour = colores[1], size = 1) +
+  # geom_point(aes(x = media_muestra_A, y = media_muestra_B), colour = colores[1], size = 1) +
   annotate("point", media_pob_A, media_pob_B, colour = colores[4], size = 5) +
   geom_abline(intercept = 0, slope = 1, colour = colores[4], size = 1) +
-  xlim(min_A, max_A) +
-  ylim(min_B, max_B) +
+  # xlim(min_A, max_A) +
+  # ylim(min_B, max_B) +
   theme_minimal()
 
 
