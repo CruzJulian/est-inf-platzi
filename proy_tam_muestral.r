ad47e3f16a5d644cfc5150e9daf5a48f24c8e336
Julián Cruz <cruz.julian.mauricio@gmail.com>
2020-08-05 22:49:32 -0500

    Perfeccionamiento
    Escribiendo el puntaje contextualizado

--- proy_tam_muestral.r
+++ proy_tam_muestral.r
@@ -4,81 +4,106 @@
 
 # Paquetes ----------------------------------------------------------------
 
-library("dplyr")
 library("saber")
 library("nnet")
 library("caret")
 library("parallel")
-library("purrr")
-library("magrittr")
 
 data("SB11_20112")
 
 
 # función de pliegue ------------------------------------------------------
 
-rmse_fold <- function(pliegue, X, Y, nn_size){
-  pliegue_logic <- seq_len(nrow(X)) %in% pliegue
-  entrena_X <- subset(X, !pliegue_logic)
-  entrena_Y <- subset(Y, !pliegue_logic)
-  modelo <- nnet(X, Y, size = nn_size, linout = TRUE, trace = FALSE)
-  Y_pronosticado <- as.vector(predict(modelo, newdata = subset(X, pliegue_logic)))
-  rmse <- sqrt(mean((unlist(subset(Y, pliegue_logic)) - Y_pronosticado)^2))
+rmse_fold <- function(pliegue, form, datos,  nn_size){
+  pliegue_logic <- seq_len(nrow(datos)) %in% pliegue
+  entrena <- subset(datos, !pliegue_logic)
+  prueba <- subset(datos, pliegue_logic)
+  modelo <- nnet(form, data = datos, size = nn_size, linout = TRUE, trace = FALSE)
+  response_name <- setdiff(names(datos), modelo$coefnames)
+  Y_pronosticado <- predict(modelo, newdata = prueba)
+  rmse <- RMSE(Y_pronosticado, prueba[[response_name]])
   rmse
 }
+
+
+calcula_rmse_tam <- function(tamano_muestral){
+  
+  indices_muestra <- seq_len(nrow(SB11_20112)) %in% sample(seq_len(nrow(SB11_20112)), tamano_muestral)
+  
+  muestra <- subset(SB11_20112, subset = indices_muestra, select = variables)
+  muestra <- na.omit(muestra)
+  
+  createFolds(muestra$MATEMATICAS_PUNT, k = n_pliegues) -> pliegues
+  
+  lapply(
+    pliegues,
+    rmse_fold, 
+    MATEMATICAS_PUNT ~., 
+    muestra, 
+    nn_size = neuronas 
+  ) -> rmse_pliegues
+  mean(unlist(rmse_pliegues))
+}
+
 
 # Red neuronal ------------------------------------------------------------
 
 
-tamano_muestral <- 2000
-n_pliegues <- 10
-tamanos_muestrales <- seq(2000, 10000, by = 200)
-n_pliegues <- 20
-neuronas <- 4
+tamano_muestral_max <- 5000
+iteraciones <- 20
+tamano_muestral <- floor(seq(500, tamano_muestral_max, length.out = iteraciones))
+n_pliegues <- 4
+neuronas <- 20
+
 
 c(
-  "ECON_SN_TELEFONIA", 
-  "ECON_SN_CELULAR",
-  "ECON_SN_INTERNET",
-  "ECON_SN_COMPUTADOR"
-) -> nombres_X
+  "ECON_PERSONAS_HOGAR",
+  "ECON_CUARTOS",
+  "ECON_SN_LAVADORA",
+  "ECON_SN_NEVERA",
+  "ECON_SN_HORNO",
+  "ECON_SN_DVD",
+  "ECON_SN_MICROHONDAS",
+  "ECON_SN_AUTOMOVIL",
+  "MATEMATICAS_PUNT"
+) -> variables
 
-"MATEMATICAS_PUNT" -> nombre_Y
 
-SB11_20112 %>% 
-  sample_n(tamano_muestral) %>% 
-  extract(c(nombres_X, nombre_Y)) %>% 
-  na.omit() -> muestra
+mclapply(
+  tamano_muestral, 
+  calcula_rmse_tam, 
+  mc.cores = floor(detectCores()*0.8)
+  ) -> rmse_por_tam
 
-X <- muestra[nombres_X]
+plot(tamano_muestral, rmse_por_tam, ylim = c(0, 12))
 
-Y <- muestra[nombre_Y]
 
-Y_v <- unlist(Y)
 
-neuronas <- 8
+# Funcionamiento ----------------------------------------------------------
 
-createFolds(Y_v, k = n_pliegues) -> pliegues
+tamano_muestral <- 5000
 
-calcula_rmse_tam <- function(tamano_muestral){
-  
-  SB11_20112 %>% 
-    sample_n(tamano_muestral) %>% 
-    extract(c(nombres_X, nombre_Y)) %>% 
-    na.omit() -> muestra
-  
-  X <- muestra[nombres_X]
-  Y <- muestra[nombre_Y]
-  Y_v <- unlist(Y)
-  
-  createFolds(Y_v, k = n_pliegues) -> pliegues
-  
-  pliegues %>% 
-    map_dbl(rmse_fold, X, Y, nn_size = neuronas) %>% 
-    mean 
-  
-}
+c(
+  "ECON_PERSONAS_HOGAR",
+  "ECON_CUARTOS",
+  "ECON_SN_LAVADORA",
+  "ECON_SN_NEVERA",
+  "ECON_SN_HORNO",
+  "ECON_SN_DVD",
+  "ECON_SN_MICROHONDAS",
+  "ECON_SN_AUTOMOVIL",
+  "MATEMATICAS_PUNT"
+) -> variables
 
-mclapply(tamanos_muestrales, calcula_rmse_tam, mc.cores = floor(detectCores()*0.8)) -> rmse_por_tam
+indices_muestra <- seq_len(nrow(SB11_20112)) %in% sample(seq_len(nrow(SB11_20112)), tamano_muestral)
 
-plot(tamanos_muestrales, rmse_por_tam)
+muestra <- subset(SB11_20112, subset = indices_muestra, select = variables)
+muestra <- na.omit(muestra)
+
+red_neuronal <- nnet(MATEMATICAS_PUNT ~., data = muestra, size = neuronas, linout = TRUE) 
+
+predict(red_neuronal, newdata = SB11_20112) -> puntaje_pronosticado
+
+nuevo_puntaje_mat <- SB11_20112$MATEMATICAS_PUNT - puntaje_pronosticado
+
+plot(density(na.omit(nuevo_puntaje_mat)))
