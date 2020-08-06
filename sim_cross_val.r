e6de48057c073ba2ceaa6a43840701b343fa5dc5
Julián Cruz <cruz.julian.mauricio@gmail.com>
2020-08-05 22:56:32 -0500

    Perfeccionamiento
    Últimos ajustes

--- sim_cross_val.r
+++ sim_cross_val.r
@@ -11,45 +11,51 @@ library("parallel")
 
 # función de pliegue ------------------------------------------------------
 
-rmse_por_pliegue <- function(pliegue, X, Y){
-  entrena_X <- X[-pliegue]
-  entrena_Y <- Y[-pliegue]
-  modelo <- nnet(X, Y, size = 8, linout = TRUE, trace = FALSE)
-  Y_pronosticado <- as.vector(predict(modelo, newdata = data.frame(X[pliegue])))
-  rmse <- sqrt(mean((Y[pliegue] - Y_pronosticado)^2))
+rmse_fold <- function(pliegue, form, datos,  nn_size){
+  pliegue_logic <- seq_len(nrow(datos)) %in% pliegue
+  entrena <- subset(datos, !pliegue_logic)
+  prueba <- subset(datos, pliegue_logic)
+  modelo <- nnet(form, data = datos, size = nn_size, linout = TRUE, trace = FALSE)
+  response_name <- setdiff(names(datos), modelo$coefnames)
+  Y_pronosticado <- predict(modelo, newdata = prueba)
+  rmse <- RMSE(Y_pronosticado, prueba[[response_name]])
   rmse
 }
+
 
 # Red neuronal ------------------------------------------------------------
 
 
 n_pliegues <- 5
 tamano_muestral <- 3000
+neuronas <- 10
 
 genera_y <- function(x){
   cos(x) + rnorm(length(x), 0, 0.5)
 }
 
-
 X <- seq(0, 3*pi, length.out = tamano_muestral)
 Y <- genera_y(X)
 
+data.frame(X, Y) -> muestra
 
-createFolds(Y, k = n_pliegues) -> pliegues
+
+createFolds(muestra$Y, k = n_pliegues) -> pliegues
 
 mclapply(
   pliegues,
-  rmse_por_pliegue,
-  X,
-  Y,
+  rmse_fold, 
+  Y ~ X,
+  muestra, 
+  nn_size = neuronas, 
   mc.cores = floor(detectCores()*0.8)
-  ) -> rmse_pliegues 
+) -> rmse_pliegues
 
 rmse_pliegues <- unlist(rmse_pliegues)
+mean(rmse_pliegues)
 
 plot(rmse_pliegues, ylim = c(0, 1))
-
-mean(rmse_pliegues)
+abline(h = mean(rmse_pliegues), col = 2, lwd = 2)
 
 
 # Tidy approach -----------------------------------------------------------
@@ -59,14 +65,16 @@ library("magrittr")
 
 n_pliegues <- 5
 tamano_muestral <- 3000
+neuronas <- 10
 
 tibble(
-  pliegues = createFolds(Y, k = n_pliegues),
+  pliegues = createFolds(muestra$Y, k = n_pliegues),
   rmse_pliegues = mclapply(
     pliegues,
-    rmse_por_pliegue,
-    X,
-    Y,
+    rmse_fold, 
+    Y ~ X,
+    muestra, 
+    nn_size = neuronas, 
     mc.cores = floor(detectCores()*0.8)
   ) %>% unlist,
   nombres = names(pliegues)
