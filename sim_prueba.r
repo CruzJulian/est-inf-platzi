5aa8e9e22dada5d0ba03817ba71c3c1b7a5ae31e
Julián Cruz <cruz.julian.mauricio@gmail.com>
2020-08-05 20:24:38 -0500

    Perfeccionamiento
    Últimos ajustes

--- sim_prueba.r
+++ sim_prueba.r
@@ -1,20 +1,18 @@
 
-# Simular la misma prueba de hipótesis con distintas condiciones e --------
 
-
-# Paquetes ----------------------------------------------------------------
+# Revisemos la potencia de una prueba. ------------------------------------
 
 
 # wmw con dos gamas -------------------------------------------------------
 
 # La media de una gamma es shape/rate, vamos a mover el shape -------------
 
-tamano_muestral <- 10
-iteraciones <- 200
+tamano_muestral <- 50
+iteraciones <- 80
 dif_media_ini <- 0
-dif_media_fin <- 3
+dif_media_fin <- 2
 media_x <- 1
-n_pasos <- 20
+n_pasos <- 50
 umbral_significancia <- 0.05
 dif_medias <- seq(dif_media_ini, dif_media_fin, length.out = n_pasos)
 
@@ -32,9 +30,9 @@ for(k in seq_along(dif_medias)){
   p_valores <- vector()
   
   for(i in seq_len(iteraciones)){
-    x <- rgamma(tamano_muestral, 1, 1)
+    x <- rgamma(tamano_muestral, media_x, 1)
     y <- rgamma(tamano_muestral, sim_shape, 1)
-    p_valores[i] <- wilcox.test(x, y) %$% p.value
+    p_valores[i] <- wilcox.test(x, y)$p.value
     
   }
   
@@ -56,9 +54,9 @@ for(k in seq_along(dif_medias)){
   p_valores <- vector()
   
   for(i in seq_len(iteraciones)){
-    x <- rgamma(tamano_muestral, 1, 1)
+    x <- rgamma(tamano_muestral, media_x, 1)
     y <- rgamma(tamano_muestral, sim_shape, 1)
-    p_valores[i] <- wilcox.test(x, y) %$% p.value
+    p_valores[i] <- t.test(x, y)$p.value
     
   }
   
@@ -75,19 +73,66 @@ lines(dif_medias, potencia_t, col = 2)
 
 # Tidy approach -----------------------------------------------------------
 
+# Paquetes ----------------------------------------------------------------
 
 
 library("ggplot2")
 library("dplyr")
+library("tidyr")
 library("magrittr")
+library("purrr")
 
+tamano_muestral <- 50
+iteraciones <- 80
+dif_media_ini <- 0
+dif_media_fin <- 2
+media_x <- 1
+n_pasos <- 50
+umbral_significancia <- 0.05
+dif_medias <- seq(dif_media_ini, dif_media_fin, length.out = n_pasos)
+
+
+
+fun_potencia_wmw <- function(shape_1, shape_2, iteraciones){
+  map_dbl(
+    seq_len(iteraciones), 
+    function(i){
+      x <- rgamma(tamano_muestral, shape_1, 1)
+      y <- rgamma(tamano_muestral, shape_2, 1)
+      wilcox.test(x, y) %$% p.value
+    }
+  ) -> p_valores
+  mean(p_valores < umbral_significancia)
+}
+
+
+fun_potencia_t <- function(shape_1, shape_2, iteraciones){
+  
+  map_dbl(
+    seq_len(iteraciones), 
+    function(i){
+      x <- rgamma(tamano_muestral, shape_1, 1)
+      y <- rgamma(tamano_muestral, shape_2, 1)
+      t.test(x, y) %$% p.value
+    }
+  ) -> p_valores
+  mean(p_valores < umbral_significancia)
+}
 
 tibble(
-  dif_medias = rep(dif_medias, 2),
-  potencia = c(potencia_t, potencia_wmw),
-  prueba = c(rep("t-Student", n_pasos), rep("Wilcoxon-Mann-Whitney", n_pasos))
-) %>% 
+  dif_medias,
+  shape_01 = media_x,
+  shape_02 = shape_01 + dif_medias,
+  potencia_wmw = map2_dbl(shape_01, shape_02, fun_potencia_wmw, iteraciones),
+  potencia_t = map2_dbl(shape_01, shape_02, fun_potencia_t, iteraciones),
+) -> simulaciones
+
+
+simulaciones %>% 
+  gather("prueba", "potencia", potencia_wmw, potencia_t) %>% 
   ggplot +
-  geom_line(aes(x = dif_medias, y = potencia, colour = prueba), size = 1) + 
-  theme_minimal()
+  geom_line(aes(x = dif_medias, y = potencia, colour = prueba), size = 0.3) +
+  geom_smooth(aes(x = dif_medias, y = potencia, colour = prueba), size = 1, se = FALSE) + 
+  theme_minimal() +
+  theme(legend.position = "bottom")
   
